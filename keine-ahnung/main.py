3cc592a8fdf84523eb46fbe54b45b82624c1e05f
asdiadenpotatisen <69951589+asdiadenpotatisen@users.noreply.github.com>
2020-09-17 13:25:47 +0800

    Update main.py

--- keine-ahnung/main.py
+++ keine-ahnung/main.py
@@ -1,2 +1,8 @@
-print("Please enter a number")
-num = input(float)
+import random
+
+print("mac quotes")
+mac_quotes = ["1", "2", "3", "4", "5"]
+
+random_num = rand(0, 4)
+mac_quote = mac_quotes[random_num]
+print("mac quote number %d" %(mac_quote))
