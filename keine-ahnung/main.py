2e7f6b873a15eaa5837d79da3185c02e954e8373
asdiadenpotatisen <69951589+asdiadenpotatisen@users.noreply.github.com>
2020-09-18 22:43:11 +0800

    Delete main.py

--- keine-ahnung/main.py
+++
@@ -1,8 +0,0 @@
-import random
-
-print("mac quotes")
-mac_quotes = ["1", "2", "3", "4", "5"]
-
-random_num = rand(0, 4)
-mac_quote = mac_quotes[random_num]
-print("mac quote number %d" %(mac_quote))
